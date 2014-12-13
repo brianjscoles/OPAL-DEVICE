5f2565d098401a5b38868ba3b13c049ef8f0f2e6
thejsj <jorge.silva@thejsj.com>
2014-12-12 16:25:18 -0800

    Fix production config

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -4,16 +4,22 @@
 var config = {};
 
 if (process.env.NODE_ENV === 'production') {
+  console.log('Production Configuration');
   config = {
     ip: process.env.IP || undefined,
     port: process.env.PORT || process.env.SERVER_PORT || 3000,
-    uri: 'mongodb://omnigraph:jGSAfs2AxzSfUBcdGOpy_IbAvI.7PE9y8P74a_lDzSg-@ds030817.mongolab.com:30817/omnigraph'
+    mongo: {
+      uri: 'mongodb://omnigraph:jGSAfs2AxzSfUBcdGOpy_IbAvI.7PE9y8P74a_lDzSg-@ds030817.mongolab.com:30817/omnigraph'
+    }
   };
 } else {
+  console.log('Local Configuration');
   config = {
     ip: undefined,
     port: 3000,
-    uri: 'mongodb://localhost/omnigrahm'
+    mongo: {
+      uri: 'mongodb://localhost/omnigrahm'
+    }
   };
 }
 
