855ca58203137d9cf78d03fb86ddea05d79fdc9a
thejsj <jorge.silva@thejsj.com>
2014-12-12 16:09:18 -0800

    Remove gulp

--- deploy.sh
+++ deploy.sh
@@ -128,14 +128,14 @@ if [ -e "$DEPLOYMENT_TARGET/bower.json" ]; then
 fi
 
 # 5. Run gulp
-if [ -e "$DEPLOYMENT_TARGET/gulpfile.js" ]; then
-  cd "$DEPLOYMENT_TARGET"
-  eval $NPM_CMD install gulp
-  exitWithMessageOnError "installing gulp failed"
-  ./node_modules/.bin/gulp
-  exitWithMessageOnError "gulp failed"
-  cd - > /dev/null
-fi
+# if [ -e "$DEPLOYMENT_TARGET/gulpfile.js" ]; then
+#   cd "$DEPLOYMENT_TARGET"
+#   eval $NPM_CMD install gulp
+#   exitWithMessageOnError "installing gulp failed"
+#   ./node_modules/.bin/gulp
+#   exitWithMessageOnError "gulp failed"
+#   cd - > /dev/null
+# fi
 ##################################################################################################################################
 
 # Post deployment stub
